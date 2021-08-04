92fee040cd6b23bc587f5759a81921ecc260335b
EC <chaosisharmonyofficial@gmail.com>
2021-08-04 20:11:59 +0800

    worked more on chap 6 Haskell; updated curriculum

--- Textbooks/Haskell/chap6.hs
+++ Textbooks/Haskell/chap6.hs
@@ -1,5 +1,6 @@
-import Data.Char
-import Data.List 
+import qualified Data.Map as Map
+import           Data.Char
+import           Data.List 
 -- To import only specific functions:
 -- 		import Data.List (nub, sort)
 -- To import all except for some specific functions:
@@ -51,3 +52,77 @@ decode' shift msg = encode (negate shift) msg
 --		foldl' from the Data.List module is supposed to fix this
 exSO = foldl (+) 0 (replicate 100000000000 1)
 exSO' = foldl' (+) 0 (replicate 1000000000 1)
+
+
+-- first natural number such that the sum of its digits equals 40
+-- my attempt
+splitDigits :: Int -> [Int]
+splitDigits 0 = []
+splitDigits x = (x `mod` 10) : splitDigits (x `div` 10)
+
+sumDigits :: Int -> Int
+sumDigits x = sum $ splitDigits x
+
+findNumSumsTo :: Int -> Int
+findNumSumsTo limit = head [x | x <- [1..], (sumDigits x) == limit]
+
+
+-- book version
+-- Uses find function from Data.List
+-- 		find :: (a -> Bool) -> [a] -> Maybe a
+-- 		Maybe type is of either size 0 or size 1
+-- 		returns Nothing or Just a
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40) [1..]
+
+
+-- Lookup
+phoneBook = [
+    ("bobo", "1"),
+    ("boba", "2"),
+    ("bobi", "3"),
+    ("bobu", "4"),
+    ("bobe", "5")
+    ]
+
+-- throws RuntimeError if key not in list
+findKey :: (Eq k) => k -> [(k, v)] -> v
+findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs
+
+-- better version
+findKey' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey' key [] = Nothing
+findKey' key ((k, v):xs)
+    | key == k  = Just v
+    | otherwise = findKey' key xs
+
+-- as a fold
+findKey'' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey'' key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
+
+
+-- fromList :: (Ord k) => [(k, v)] -> Map.Map k v
+phoneBookMap = Map.fromList $ phoneBook
+
+
+-- Map.lookup :: (Ord k) => k -> Map.Map k a -> Maybe a
+-- Map.insert :: (Ord k) => k -> Map.Map k a -> Map.Map k a
+-- Map.size :: Map.Map k a -> Int
+
+
+-- Map.fromListWith
+addValues = Map.fromListWith (+) [(1, 2), (1, -2), (2, 0), (3, 1), (3, -1)]
+
+
+
+
+
+
+
+
+
+
+
