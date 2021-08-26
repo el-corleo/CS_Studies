d8c5fe6cff450478ac8dc65cb4b55ee2e7c65a0d
EC <chaosisharmonyofficial@gmail.com>
2021-08-26 20:09:10 +0800

    finished chap8 haskell

--- Textbooks/Haskell/chap8.hs
+++ Textbooks/Haskell/chap8.hs
@@ -1,6 +1,7 @@
 -- To compile a Haskell Program
 -- $ ghc --make <filename>
 import Data.Char
+import Control.Monad
 
 -- main = putStrLn "Hello, World!"
 
@@ -20,13 +21,34 @@ import Data.Char
 											-- ++ upperLastName ++ " "
 											-- ++ ", how are you?"
 
-main = do
-	line <- getLine
-	if null line
-		then return ()
-		else do
-			putStrLn $ reverseWords line
-			main
+-- main = do
+	-- line <- getLine
+	-- if null line
+		-- then return ()
+		-- else do
+			-- putStrLn $ reverseWords line
+			-- main
+--
+-- reverseWords :: String -> String
+-- reverseWords = unwords . map reverse . words
 
-reverseWords :: String -> String
-reverseWords = unwords . map reverse . words
+-- from Control.Monad we get 'when'
+-- main = do
+	-- input <- getLine
+	-- when (input == "SWORDFISH") $ do
+		-- putStrLn input
+
+-- sequence takes a sequence of IO actions and stores them in a variable
+-- main = do
+	-- rs <- sequence [getLine, getLine, getLine]
+	-- print rs
+	-- -- another example of how to use it is when mapping functions over lists
+	-- sequence $ map print [1,2,3,4]
+
+main = do
+	colors <- forM [1,2,3,4] (\a -> do
+		putStrLn $ "What color do you associate with " ++ show a ++ "?"
+		color <- getLine
+		return color)
+	putStrLn "Colors for 1,2,3,4:"
+	mapM putStrLn colors
