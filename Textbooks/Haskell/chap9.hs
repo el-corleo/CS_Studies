5d2af107a52c41a7b7c59e9125b1a201e83aa0f0
EC <chaosisharmonyofficial@gmail.com>
2021-08-30 20:22:38 +0800

    did some more from haskell textbook

--- Textbooks/Haskell/chap9.hs
+++ Textbooks/Haskell/chap9.hs
@@ -1,6 +1,7 @@
 -- dealing with input redirection (e.g., reading from a .txt file)
 import Control.Monad
 import Data.Char
+import System.IO
 
 -- main = forever $ do
 	-- l <- getLine
@@ -25,19 +26,37 @@ import Data.Char
 -- longLinesOnly = unlines . filter (\line -> length line > 10) . lines
 
 -- Check if is palindrome
-main = interact respondPalindrome
-
-respondPalindrome :: String -> String
-respondPalindrome =
-	unlines .
-	map (\xs -> if isPal xs then "palindrome" else "is not palindrome") .
-	lines
-
-isPal :: String -> Bool
-isPal []			= True
-isPal [x]			= True
-isPal (x:xs)	= x == (head $ reverse xs) && isPal (init xs)
+-- main = interact respondPalindrome
+--
+-- respondPalindrome :: String -> String
+-- respondPalindrome =
+	-- unlines .
+	-- map (\xs -> if isPal xs then "palindrome" else "is not palindrome") .
+	-- lines
+--
+-- isPal :: String -> Bool
+-- isPal []			= True
+-- isPal [x]			= True
+-- isPal (x:xs)	= x == (head $ reverse xs) && isPal (init xs)
 
 -- book version of isPal
 -- isPal :: String -> Bool
 -- isPal xs = xs == reverse xs
+
+
+-- reading a file from stdin
+main = do
+	handle <- openFile "randFile.txt" ReadMode
+	contents <- hGetContents handle
+	putStr contents
+	hClose handle
+
+-- "Just like getContents, hGetContents won’t attempt to read all the file at
+-- once and store it in memory but will read the content only as needed."
+	-- Thought: so does this make it good for data processing tasks?
+	-- Another thought: since it appears to read from disk, would this make it really slow?
+
+-- "Note the difference between a handle and the actual contents of the file. A handle just points to our current position in the file. The contents are what’s actually in the file."
+
+
+
