5445c5e47df2f27c7ee25db1e394df7ddc1436a4
EC <chaosisharmonyofficial@gmail.com>
2021-09-01 08:28:55 +0800

    finished more of chap9 haskell

--- Textbooks/Haskell/chap9.hs
+++ Textbooks/Haskell/chap9.hs
@@ -3,45 +3,45 @@ import Control.Monad
 import Data.Char
 import System.IO
 
--- main = forever $ do
-	-- l <- getLine
-	-- putStrLn $ map toUpper l
+main = forever $ do
+	l <- getLine
+	putStrLn $ map toUpper l
 
 -- does the same as above
--- main = do
-	-- contents <- getContents
-	-- putStr $ map toUpper contents
-
--- main = do
-	-- contents <- getContents
-	-- putStr (shortLinesOnly contents)
+main = do
+	contents <- getContents
+	putStr $ map toUpper contents
 --
--- shortLinesOnly :: String -> String
--- shortLinesOnly = unlines . filter (\line -> length line < 10) . lines
+main = do
+	contents <- getContents
+	putStr (shortLinesOnly contents)
+
+shortLinesOnly :: String -> String
+shortLinesOnly = unlines . filter (\line -> length line < 10) . lines
 
 -- interact takes a function that takes a string from input and returns a string
--- main = interact longLinesOnly
---
--- longLinesOnly :: String -> String
--- longLinesOnly = unlines . filter (\line -> length line > 10) . lines
+main = interact longLinesOnly
+
+longLinesOnly :: String -> String
+longLinesOnly = unlines . filter (\line -> length line > 10) . lines
 
 -- Check if is palindrome
--- main = interact respondPalindrome
---
--- respondPalindrome :: String -> String
--- respondPalindrome =
-	-- unlines .
-	-- map (\xs -> if isPal xs then "palindrome" else "is not palindrome") .
-	-- lines
---
--- isPal :: String -> Bool
--- isPal []			= True
--- isPal [x]			= True
--- isPal (x:xs)	= x == (head $ reverse xs) && isPal (init xs)
+main = interact respondPalindrome
+
+respondPalindrome :: String -> String
+respondPalindrome =
+	unlines .
+	map (\xs -> if isPal xs then "palindrome" else "is not palindrome") .
+	lines
+
+isPal :: String -> Bool
+isPal []			= True
+isPal [x]			= True
+isPal (x:xs)	= x == (head $ reverse xs) && isPal (init xs)
 
 -- book version of isPal
--- isPal :: String -> Bool
--- isPal xs = xs == reverse xs
+isPal :: String -> Bool
+isPal xs = xs == reverse xs
 
 
 -- reading a file from stdin
@@ -59,4 +59,18 @@ main = do
 -- "Note the difference between a handle and the actual contents of the file. A handle just points to our current position in the file. The contents are what’s actually in the file."
 
 
+-- withFile makes sure file gets closed
+main = do
+	withFile "randFile.txt" ReadMode (\handle -> do
+		contents <- hGetContents handle
+		putStr contents)
 
+
+-- terser way to read a file; automatically creates handle and reads it to contents; also closes fileHandle automatically
+main = do
+	contents <- readFile "randFile.txt"
+	putStr contents
+
+-- "We could just open a normal file called todo2.txt or something like that, but it’s better practice to use openTempFile so you know you’re probably not overwriting anything."
+
+-- "The System.Environment module has two cool I/O actions that are useful for getting command-line arguments: getArgs and getProgName."
